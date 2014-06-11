4cae38cbaef728ad7f51788b0675f5607448061e
Jessica Oei <jessicalikescameras@gmail.com>
2014-06-11 17:30:50 -0400

    load specific post based on qs

--- index.php
+++ index.php
@@ -5,27 +5,53 @@
 	<title>crappyblog</title>
 	<?php require_once('header.php'); ?>
 </head>
-<body class="home">
+<body class="post">
 	<?php require_once('topnav.php'); ?>
 
-	<?php 
-		$conf = parse_ini_file('conf.ini');		
-		$hostname = $conf['hostname'];
-		$username = $conf['username'];
-		$password = $conf['password'];		
-		mysql_connect($hostname,$username,$password) or die(mysql_error()); 
-		mysql_select_db('crappyblog') or die(mysql_error());
-	?>
 	<?php
-		$query = mysql_query('SELECT * FROM posts ORDER BY date DESC LIMIT 1') or die(mysql_error());
-		while($post = mysql_fetch_array($query))
-		{
-			echo '<div class="row"><div class="small-12 large-12 columns">';
-				echo '<h2>' . $post['title'] . '</h2><p>' . $post['date'] . '</p>'. $post['content'];
-			echo '</div></div>';				
-		} 
+		//echo "qs(" . $_SERVER['QUERY_STRING'] . ")";
 
+		// check for querystring to load specific post
+		if($_SERVER['QUERY_STRING'] != "")
+		{
+			parse_str($_SERVER['QUERY_STRING'], $qs);
+			//echo $qs['id'];
+
+			$query = mysql_query("SELECT * FROM posts WHERE id = " . $qs['id']) or die(mysql_error());
+
+			while($post = mysql_fetch_array($query))
+			{
+				$postdate = new DateTime($post['date']);
+
+				echo '<div class="row"><div class="small-12 large-12 columns">';
+					echo '<h2>' . $post['title'] . '</h2>';				
+					echo '<p class="date">' . $postdate->format('y.m.d @ H:m') . '</p>';
+					echo $post['content'];
+				echo '</div></div>';				
+			} 
+		}
+		//	otherwise load most recent post
+		else 
+		{	
+			$query = mysql_query('SELECT * FROM posts ORDER BY date DESC LIMIT 1') or die(mysql_error());
+
+			while($post = mysql_fetch_array($query))
+			{
+				$postdate = new DateTime($post['date']);
+
+				echo '<div class="row"><div class="small-12 large-12 columns">';
+					echo '<h2>' . $post['title'] . '</h2>';				
+					echo '<p class="date">' . $postdate->format('y.m.d @ H:m') . '</p>';
+					echo $post['content'];
+				echo '</div></div>';				
+			} 
+		}
 	?> 	
+
+	<!--
+	crappyblog.local.com?id=1
+	crappyblog.local.com/posts/learning-the-language-of-coding
+-->
 
 	<?php require_once('footer.php'); ?>
 	
